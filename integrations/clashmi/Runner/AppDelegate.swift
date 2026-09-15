b05cbd4e3a7f54ec3a64280d9b9df1d79652a1c0
xepes0 <kevin.h.cen@gmail.com>
2026-09-15 21:21:06 +0800

    feat: intercept clashmi WLOC deep links

---
+++ integrations/clashmi/Runner/AppDelegate.swift
@@ -0,0 +1,27 @@
+import Flutter
+import UIKit
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    if WLOCMainBridge.shared.handle(url: url) {
+      return true
+    }
+    return super.application(app, open: url, options: options)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+}
