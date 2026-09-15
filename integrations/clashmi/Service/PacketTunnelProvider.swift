65f12f3dcaf36a4283e6bab25bf514c526e14e85
xepes0 <kevin.h.cen@gmail.com>
2026-09-15 21:20:54 +0800

    feat: route WLOC messages in Clash Mi PacketTunnel

---
+++ integrations/clashmi/Service/PacketTunnelProvider.swift
@@ -0,0 +1,22 @@
+import Foundation
+import LibVpnCore
+
+class PacketTunnelProvider: ExtensionProvider {
+    private let wlocBridge = WLOCTunnelBridge()
+
+    override func handleAppMessage(_ messageData: Data) async -> Data? {
+        if wlocBridge.handles(messageData) {
+            return await wlocBridge.handle(messageData)
+        }
+        return await super.handleAppMessage(messageData)
+    }
+
+    override func stopTunnel(
+        with reason: NEProviderStopReason,
+        completionHandler: @escaping () -> Void
+    ) {
+        // Best effort: clear LocationSimulation before the VPN extension exits.
+        wlocBridge.shutdown(timeoutSeconds: 3)
+        super.stopTunnel(with: reason, completionHandler: completionHandler)
+    }
+}
