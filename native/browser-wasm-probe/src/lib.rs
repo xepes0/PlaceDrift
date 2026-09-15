2314a178c35c3ea079f0b11853d795668fae3e1a
xepes0 <kevin.h.cen@gmail.com>
2026-09-15 22:52:06 +0800

    browser-wasm: fix wasm API surface trait bounds

--- native/browser-wasm-probe/src/lib.rs
+++ native/browser-wasm-probe/src/lib.rs
@@ -6,22 +6,24 @@ use idevice::dvt::{
     location_simulation::LocationSimulationClient,
     remote_server::RemoteServerClient,
 };
-use idevice::remote_pairing::{RemotePairingClient, RpPairingFile};
+use idevice::remote_pairing::{RemotePairingClient, RpPairingFile, RpPairingSocketProvider};
 use idevice::rsd::RsdHandshake;
 
 pub fn wasm_api_surface_marker() -> usize {
     // This crate intentionally has no socket implementation yet. The purpose of
     // this target is to prove that the exact idevice revision and CoreDevice
     // protocol surface compile for wasm32-unknown-unknown without native TCP.
-    core::mem::size_of::<RpPairingFile>()
+    core::mem::size_of::<RpPairingFile>() + core::mem::size_of::<RsdHandshake>()
 }
 
 fn _require_transport<T: ReadWrite>(_transport: &T) {}
 fn _require_rsd<T: RsdService>() {}
 
-fn _type_surface<T: ReadWrite>() {
+fn _type_surface<T>()
+where
+    T: ReadWrite + RpPairingSocketProvider,
+{
     let _ = core::mem::size_of::<Option<RemotePairingClient<T>>>();
     let _ = core::mem::size_of::<Option<RemoteServerClient<T>>>();
     let _ = core::mem::size_of::<Option<LocationSimulationClient<'static, T>>>();
-    let _ = core::mem::size_of::<Option<RsdHandshake<T>>>();
 }
