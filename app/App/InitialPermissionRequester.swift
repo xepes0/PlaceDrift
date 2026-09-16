4f17d1aed753f0d6f38ed30058bfc732f43432ae
xepes0 <kevin.h.cen@gmail.com>
2026-09-16 20:44:24 +0800

    Add first-launch location permission flow

---
+++ app/App/InitialPermissionRequester.swift
@@ -0,0 +1,47 @@
+import CoreLocation
+import Foundation
+
+@MainActor
+final class InitialPermissionRequester: NSObject, ObservableObject, @preconcurrency CLLocationManagerDelegate {
+    private static let whenInUseRequestKey = "placedrift.permissions.when-in-use-requested"
+    private static let alwaysRequestKey = "placedrift.permissions.always-requested"
+
+    private let manager = CLLocationManager()
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func requestIfNeeded() {
+        guard CLLocationManager.locationServicesEnabled() else { return }
+
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            guard !UserDefaults.standard.bool(forKey: Self.whenInUseRequestKey) else { return }
+            UserDefaults.standard.set(true, forKey: Self.whenInUseRequestKey)
+            manager.requestWhenInUseAuthorization()
+
+        case .authorizedWhenInUse:
+            requestAlwaysIfNeeded()
+
+        case .authorizedAlways, .denied, .restricted:
+            break
+
+        @unknown default:
+            break
+        }
+    }
+
+    private func requestAlwaysIfNeeded() {
+        guard !UserDefaults.standard.bool(forKey: Self.alwaysRequestKey) else { return }
+        UserDefaults.standard.set(true, forKey: Self.alwaysRequestKey)
+        manager.requestAlwaysAuthorization()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            requestAlwaysIfNeeded()
+        }
+    }
+}
