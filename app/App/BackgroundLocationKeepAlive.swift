4797a12be35f7e2dc791f7146c6cee03581d50c3
xepes0 <kevin.h.cen@gmail.com>
2026-09-16 18:29:01 +0800

    Add silent background location keepalive

---
+++ app/App/BackgroundLocationKeepAlive.swift
@@ -0,0 +1,110 @@
+import CoreLocation
+import Foundation
+
+@MainActor
+final class BackgroundLocationKeepAlive: NSObject, @preconcurrency CLLocationManagerDelegate {
+    enum State: String, Sendable {
+        case idle
+        case requestingAlwaysAuthorization
+        case needsAlwaysAuthorization
+        case active
+        case denied
+        case restricted
+        case servicesDisabled
+        case failed
+        case stopped
+    }
+
+    private(set) var state: State = .idle
+    var onStateChange: ((State) -> Void)?
+
+    private let manager = CLLocationManager()
+    private var requested = false
+    private var requestedAlwaysUpgrade = false
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyKilometer
+        manager.distanceFilter = kCLDistanceFilterNone
+        manager.pausesLocationUpdatesAutomatically = false
+        manager.showsBackgroundLocationIndicator = false
+        manager.activityType = .other
+    }
+
+    func start() {
+        requested = true
+        reconcileAuthorization()
+    }
+
+    func stop() {
+        requested = false
+        requestedAlwaysUpgrade = false
+        manager.stopUpdatingLocation()
+        manager.allowsBackgroundLocationUpdates = false
+        update(.stopped)
+    }
+
+    private func update(_ newState: State) {
+        guard state != newState else { return }
+        state = newState
+        onStateChange?(newState)
+    }
+
+    private func reconcileAuthorization() {
+        guard requested else { return }
+        guard CLLocationManager.locationServicesEnabled() else {
+            manager.stopUpdatingLocation()
+            manager.allowsBackgroundLocationUpdates = false
+            update(.servicesDisabled)
+            return
+        }
+
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            update(.requestingAlwaysAuthorization)
+            manager.requestAlwaysAuthorization()
+
+        case .authorizedWhenInUse:
+            manager.stopUpdatingLocation()
+            manager.allowsBackgroundLocationUpdates = false
+            update(.needsAlwaysAuthorization)
+            if !requestedAlwaysUpgrade {
+                requestedAlwaysUpgrade = true
+                manager.requestAlwaysAuthorization()
+            }
+
+        case .authorizedAlways:
+            manager.showsBackgroundLocationIndicator = false
+            manager.allowsBackgroundLocationUpdates = true
+            manager.startUpdatingLocation()
+            update(.active)
+
+        case .denied:
+            manager.stopUpdatingLocation()
+            manager.allowsBackgroundLocationUpdates = false
+            update(.denied)
+
+        case .restricted:
+            manager.stopUpdatingLocation()
+            manager.allowsBackgroundLocationUpdates = false
+            update(.restricted)
+
+        @unknown default:
+            manager.stopUpdatingLocation()
+            manager.allowsBackgroundLocationUpdates = false
+            update(.failed)
+        }
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        reconcileAuthorization()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard requested else { return }
+        if (error as? CLError)?.code == .denied {
+            update(CLLocationManager.locationServicesEnabled() ? .denied : .servicesDisabled)
+        }
+    }
+}
